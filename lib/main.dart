e7fe9c61a4addd086da801ca2430d83a6bdeeab8
Brandon Prak <brandonprak@gmail.com>
2020-04-15 18:08:14 -0700

    Added secondary app page

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -93,18 +83,13 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text('Home'),
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.art_track),
-              color: Colors.white,
-              onPressed: () {},
-            ),
-            IconButton(
               icon: Icon(Icons.settings),
               color: Colors.white,
               onPressed: () {},
             ),
           ]),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Card(
             color: Colors.blueGrey,
@@ -125,8 +110,25 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _cycleSamples,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ListPage()),
+          );
+        },
         child: Icon(Icons.android),
+      ),
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[800],
+      appBar: AppBar(
+        title: Text('Second Page'),
       ),
     );
   }
